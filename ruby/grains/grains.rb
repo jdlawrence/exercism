1ed92136164d1a854ad5ca8cee9700511360b88e
Jamil Lawrence <jamillawrence@Jamils-MBP.attlocal.net>
2024-07-27 17:01:34 -0500

    Finished with grains

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -6,4 +6,12 @@ To get started with TDD, see the `README.md` file in your
 `ruby/grains` directory.
 =end
 class Grains
+  def self.square(board_size)
+    raise ArgumentError unless board_size > 0 && board_size < 65
+    2 ** (board_size - 1)
+  end
+
+  def self.total
+    2 ** 64 - 1
+  end
 end
